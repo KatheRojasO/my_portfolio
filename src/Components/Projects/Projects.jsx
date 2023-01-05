f68647ec6eadca7f6061d1e238ec89d42da73a09
Eduardo <el_alienx@hotmail.com>
2023-01-05 17:24:42 +0100

    refactored CSS in projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,30 +4,31 @@ import ProjectCard from "./ProjectCard";
 import "../Projects/Projects.css";
 
 export default function Projects() {
+  // Components
+  const Items = data.map((project) => {
+    return (
+      <ProjectCard
+        projectName={project.name}
+        projectImage={project.image}
+        description={project.description}
+        projectTech={project.tech}
+        isProjectDone={project.done}
+      />
+    );
+  });
+
   return (
-    <div className="project-section" id="projects">
-      <div className="project-info-container">
-        <h1>Projects</h1>
-        <p>
-          I've been brushing my skills as a frontend developer and here you can
-          find my last art pieces which were created during a frontend course
-          with Novare Potencial.
-        </p>
-      </div>
-      <div className="projects-cards-container">
-        <div className="projects-cards">
-          {data.map((project) => {
-            return (
-              <ProjectCard
-                projectName={project.name}
-                projectImage={project.image}
-                description={project.description}
-                projectTech={project.tech}
-                isProjectDone={project.done}
-              />
-            );
-          })}
+    <div id="projects">
+      <div className="container">
+        <div className="info-container">
+          <h2>Projects</h2>
+          <p>
+            I've been brushing my skills as a frontend developer and here you
+            can find my last art pieces which were created during a frontend
+            course with Novare Potencial.
+          </p>
         </div>
+        <div className="cards-container">{Items}</div>
       </div>
     </div>
   );
