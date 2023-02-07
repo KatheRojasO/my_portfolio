270e8cb58958f1d4e21f395382fca111fd5433cc
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-07 11:28:56 +0100

    Fix hero css for mobile

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,12 +3,16 @@ import React from "react";
 export default function Hero() {
   return (
     <section id="hero">
-      <h2 className="hero-title">
-        <span className="title-word word-1">Hi, I'm Katheryn</span>
-        <span className="title-word word-2">a </span><br/>
-        <span className="title-word word-3">Frontend Developer</span><br/>
-        <span className="title-word word-4">from Colombia</span>
-      </h2>
+      <div className="container">
+        <h1 className="hero-title">
+          <span className="title-word word-1">Hi, I'm Katheryn</span>
+          <span className="title-word word-2">a </span>
+          <br />
+          <span className="title-word word-3">Frontend Developer</span>
+          <br />
+          <span className="title-word word-4">from Colombia</span>
+        </h1>
+      </div>
     </section>
   );
 }
