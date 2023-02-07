c32a2a2f14ff60e2526ad846562bc456fa5ca9bd
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-07 20:50:13 +0100

    Fix problem with about image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,18 +5,22 @@ export default function About() {
   return (
     <div id="about">
       <div className="container">
-        <h2>About Me</h2>
-        <p>
-          Hello everybody! My name is Katheryn and I’m a junior frontend
-          developer ready to take my career to the next level.
-        </p>
-        <p>
-          During my studies I’ve developed some apps and websites, some better
-          than the others. That's why I decided to build this website to show
-          you my best projects.
-        </p>
-        <img className="about-img" src={img} alt="me-about"></img>
-      </div> 
+        <div className="paragraph-container">
+          <h2>About Me</h2>
+          <p>
+            Hello everybody! My name is Katheryn and I’m a junior frontend
+            developer ready to take my career to the next level.
+          </p>
+          <p>
+            During my studies I’ve developed some apps and websites, some better
+            than the others. That's why I decided to build this website to show
+            you my best projects.
+          </p>
+        </div>
+        <div className="img-container">
+          <img className="about-img" src={img} alt="me-about"></img>
+        </div>
+      </div>
     </div>
   );
 }
