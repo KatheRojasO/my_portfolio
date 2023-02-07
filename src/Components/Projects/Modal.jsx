11f0a14b460c16599bc06cdaf53867903c3cd9fa
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-07 15:05:53 +0100

    Fix css and refactor modal

--- src/Components/Projects/Modal.jsx
+++ src/Components/Projects/Modal.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Link } from "react-router-dom";
 const closeButton = require("../../assets/icons/close.png");
 
 export default function Modal({ open, project, onClose }) {
-  const { name, description, tech, image } = project;
+  const { name, description, tech, image, gitLink, website } = project;
 
   const modalImage = require(`../../assets/img/${image}`);
 
   if (!open) return null;
 
+  const techOptions = tech.map((tech) => {
+    return (
+      <li>
+        <span className="tech-options">{tech}</span>
+      </li>
+    );
+  });
+
   return ReactDOM.createPortal(
     <div>
       <div className="overlay-style" />
       <div className="modal-style">
-        <img className='close-button' src={closeButton} alt="not found" onClick={onClose} />
+        <img
+          className="close-button"
+          src={closeButton}
+          alt="not found"
+          onClick={onClose}
+        />
         <div className="modal-container">
-          <img src={modalImage} alt="not found" />
-          <div className="modal-info">
+          <img className="modal-img" src={modalImage} alt="project-img" />
+          <div className="container">
             <h2>{name}</h2>
-            <ul className="tech-list">
-              {tech.map((tech) => {
-                return (
-                  <li>
-                    <span className="tech-options">{tech}</span>
-                  </li>
-                );
-              })}
-            </ul>
+            <ul className="tech-list">{techOptions}</ul>
             <p className="description">{description}</p>
-            <button className="btn-app"> Visit website/App</button>
-            <button className="btn-git"> Git repository</button>
+            <a className="web-link" href={website} target="_blank" rel="noopener noreferrer">
+              Visit the website
+            </a>
+            <a className="git-link" href={gitLink} target="_blank" rel="noopener noreferrer">
+              Git repository
+            </a>
           </div>
         </div>
       </div>
