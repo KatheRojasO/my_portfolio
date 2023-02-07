ee5300e9a6a3258ec0d73593f12779f3ddf9c6da
KatheRojasO <79369847+KatheRojasO@users.noreply.github.com>
2023-02-07 17:50:56 +0100

    Add links to footer icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import linkedinIcon from '../../assets/icons/linkedin.png';
-import githubIcon from '../../assets/icons/github.png';
-import instagramICon from '../../assets/icons/instagram.png';
-import copyICon from '../../assets/icons/copy.png';
-
+import linkedinIcon from "../../assets/icons/linkedin.png";
+import githubIcon from "../../assets/icons/github.png";
+import instagramICon from "../../assets/icons/instagram.png";
+import copyICon from "../../assets/icons/copy.png";
 
 export default function Footer() {
   return (
     <div className="footer-section">
       <div className="social-media">
-        <img src={linkedinIcon} alt='linkedin-icon'/>
-        <img src={githubIcon} alt='gitgub-icon'/>
-        <img src={instagramICon} alt='instagram-icon'/>
+        <a href="https://www.linkedin.com/in/katherynrojas/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinIcon} alt="linkedin-icon" />
+        </a>
+        <a href="https://github.com/KatheRojasO" target="_blank" rel="noopener noreferrer">
+          <img src={githubIcon} alt="gitgub-icon" />
+        </a>
+        <img src={instagramICon} alt="instagram-icon" />
       </div>
       <div className="copy-right">
-        <img src={copyICon} alt='instagram-icon'/> 
+        <img src={copyICon} alt="instagram-icon" />
         <p className="copy-right-text"> 2022 Katheryn Rojas </p>
       </div>
     </div>
