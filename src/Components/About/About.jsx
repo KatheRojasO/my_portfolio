5c53b5818be1f35c3e5532c36aade57b84583b91
KatheRojasO <kathe.rojaso@gmail.com>
2024-12-19 13:09:48 +0100

    Update info in about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,13 +9,16 @@ export default function About() {
           <h2 className="title-red">Introduce</h2>
           <h2>About Me</h2>
           <p>
-            Hello everybody! My name is Katheryn and I’m a junior frontend
-            developer ready to take my career to the next level.
+            Hello everybody! My name is Katheryn and I’m a Software Developer with almost 2 years of
+            experience as a Frontend Developer in different startups.
           </p>
           <p>
-            During my studies I’ve developed some apps and websites, some better
-            than the others. That's why I decided to build this website to show
-            you my best projects.
+            I have a bachelor of technology in Software Development, and recently my curiosity led me
+            to become a Java Fullstack Developer.
+          </p>
+          <p>
+            I’m passionate about coding, solving problems and I’m always looking for new challenges that allow me to
+            learn and grow as a professional. Here you can find some of my best projects and my contact information.
           </p>
         </div>
         <div className="img-container">
